fc971b8ca41cc5fd3b3f6ef56a55238aa6a334b2
bitrayne <bitrayne@gmail.com>
2015-04-08 21:56:56 -0400

    Starting with Scott Allen's tutorial

---
+++ src/aurelia_tutorial/wwwroot/app.js
@@ -0,0 +1,13 @@
+﻿export class App {
+    constructor() {
+        this.message = "";
+    }
+
+    activate() {
+        this.message = "Hello, World!";
+    }
+
+    changeMessage() {
+        this.message = "Goodbye!";
+    }
+}
